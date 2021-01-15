dd44dcb11f7e055312a97d4d5d5740d536c9be5e
Santiago Yambay <santy-yambay@hotmail.com>
2021-01-15 14:28:11 -0500

    Correccion errores 1

--- views/modules/navigation.php
+++ views/modules/navigation.php
@@ -2,8 +2,9 @@
           <ul>
               <li> <a href="index.php?action=inicio"> Inicio</a> </li>
               <li> <a href="index.php?action=nosotros"> Nosotros</a> </li>
-              <li> <a href="index.php?action=formulario"> Formulario</a> </li>
+              <li> <a href="index.php?action=servicios"> Sercicios</a> </li>
               <li> <a href="index.php?action=contactanos">
               Contactenos</a> </li>           
           </ul>
 </nav>
+
