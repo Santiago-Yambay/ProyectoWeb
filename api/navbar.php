e35b37b45e93b93893008d2de6ca21fb24b205dd
Santiago Yambay <santy-yambay@hotmail.com>
2021-01-20 20:40:40 -0500

    PANTALLA INICIAL

--- api/navbar.php
+++ api/navbar.php
@@ -9,7 +9,7 @@
   <span class="icon-bar"></span>
   <span class="icon-bar"></span>
 </button>
-<a class="navbar-brand" href="http://solucel.com.ar/app"><img src="http://solucel.com.ar/ml/logo.png"  style="width:45px; margin-top:-10px"></a>
+<a class="navbar-brand" href="http://solucel.com.ar/app"><img src="img/logo.png"  style="width:45px; margin-top:-10px"></a>
 </div>
 
 
